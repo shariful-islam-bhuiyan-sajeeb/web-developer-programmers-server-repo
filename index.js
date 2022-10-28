52f6307a198e2516286fce425620fd357aa21f86
shariful4345 <mdsajeeb4345@gmail.com>
2022-10-28 11:19:20 +0600

    index js file somthin change

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 const express = require("express");
+
 const app = express();
+
 const cors = require("cors");
 app.use(cors());
-  
-const Port = process.env.Port || 5000;
 
-const learn = require("./Data/learns.json");
+const port = process.env.port || 5000;
+
+const courses = require("./data/learns.json");
+
+app.get("/courses", (req, res) => {
+    res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedCourses = courses.find(c => c.id === id);
+    res.send(selectedCourses);
+    console.log(selectedCourses);
+});
 
 app.get("/", (req, res) => {
-    res.send("Now server is running");
+    res.send("news api running");
 });
 
-app.get("/learn", (req, res) => {
-    res.send(learn);
-});
-
-app.get("/learn/:id", (req, res) => {
-    const id = req.params.id;
-    const getSingleItem = learn?.find((p) => p.id == id);
-    if (!getSingleItem) {
-        res.send("data khuje pacchi naaa to vai");
-    }
-    res.send(getSingleItem);
-});
-
-app.get("/category/:name", (req, res) => {
-    const name = req.params.name;
-    const getLearn = learn?.filter((p) => p.category == name);
-    res.send(getLearn);
-});
-
-app.listen(Port, () => {
-    console.log("server is running", Port);
+app.listen(port, () => {
+    console.log("news serve rn", port);
 });
